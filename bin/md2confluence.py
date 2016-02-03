96fe547815fae4b45e695a0dfcb1525924666cb3
Ryosuke Mashiko <ryosuke.mashiko@dena.com>
2016-02-04 00:07:50 +0900

    Impl parse rule

--- bin/md2confluence.py
+++ bin/md2confluence.py
@@ -1,6 +1,8 @@
-import sys
-import mistune
 import argparse
+import mistune
+import re
+import sys
+import textwrap
 
 
 def _help():
@@ -16,12 +18,30 @@ def main():
     parser.add_argument('--pass')
     parser.add_argument('--id')
     parser.add_argument('--parent-id')
+
     args = parser.parse_args()
     # print(args.accumulate(args.integers))
-    html = mistune.markdown(open(args.file).read())
+
+    html = mistune.markdown(open(args.file).read(), renderer=ConfluenceRenderer())
+
     print(html)
 
-def upload(html, id=None, pid=None, user=None, pass=None):
+
+def upload(html, id=None, pid=None, user=None, password=None):
     pass
+
+
+class ConfluenceRenderer(mistune.Renderer):
+    def block_code(self, code, lang):
+        return textwrap.dedent('''\
+            <ac:structured-macro ac:name="code" ac:schema-version="1">
+                <ac:parameter ac:name="language">{l}</ac:parameter>
+                <ac:plain-text-body><![CDATA[{c}]]></ac:plain-text-body>
+            </ac:structured-macro>
+        ''').format(c=code, l=lang or '')
+
+    def linebreak(self):
+        return '<br />\n'
+
 
 main()
